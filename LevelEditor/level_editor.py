7fb690b928fce1bf1f5b02c44f70514fcade8885
Samuel Fries <fries2@llnl.gov>
2016-01-26 15:54:20 -0800

    Added Level Editor folder and skeleton script

---
+++ LevelEditor/level_editor.py
@@ -0,0 +1,69 @@
+from PySide import QtGui, QtCore
+import cdms2
+import vcs
+
+
+class LevelEditor(QtGui.QWidget):
+    levelsChanged = QtCore.Signal(list)
+
+    def __init__(self, variable, levels=None):
+        super(LevelEditor, self).__init__()
+
+        layout = QtGui.QVBoxLayout()
+        self.set_title("%s Levels" % variable.id)
+        self.variable = variable
+        self._levels = levels
+
+        self.level_label = QtGui.QLabel()
+
+        def update_level_label(levels):
+            self.level_label.setText(repr(levels))
+
+        # Connect this to your object's signal
+        def set_levels(levels):
+            self.levels = levels
+
+        self.levelsChanged.connect(update_level_label)
+
+        layout.addWidget(self.level_label)
+        self.setLayout(layout)
+
+    def show(self):
+        self.level_label.setText(repr(self.levels))
+        super(LevelEditor, self).show()
+
+    @property
+    def levels(self):
+        if self._levels is None:
+            if self.variable is not None:
+                return vcs.utils.mkscale(*vcs.minmax(self.variable))
+            else:
+                self._levels = []
+
+        return self._levels
+
+    @levels.setter
+    def set_levels(self, levels):
+        if levels is None:
+            self._levels = None
+            levels = self.levels
+
+        if levels != self._levels:
+            self._levels = levels
+            self.levelsChanged.emit(levels)
+
+    def set_title(self, title):
+        # This will get swapped out with a label later
+        self.setWindowTitle(title)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    cdmsfile = cdms2.open(vcs.sample_data + "/clt.nc")
+    clt = cdmsfile("clt")
+    le = LevelEditor(clt)
+    le.show()
+    le.raise_()
+    le.activateWindow()
+    app.exec_()
