cbd5428aeea703ec1dc4a272ce5793cab948cde8
Bryce Sampson <sampson.bryce@yahoo.com>
2016-01-28 13:44:55 -0800

    minor changes

--- CDMS_VCS/avg_cloudiness.py
+++ CDMS_VCS/avg_cloudiness.py
@@ -1,13 +1,10 @@
 import vcs, cdms2, sys, os
 
 file = cdms2.open(os.path.join(vcs.sample_data, "clt.nc"))
-clt = file("clt", time=('1985-1', '1986-1'))
-t = clt.getTime()[::3]
+clt = file("clt", time=('1985-1', '1985-12'))
 canvas = vcs.init()
-alt_c = None
-move_y = 0
-for i in range(len(t)-1):
-
+move_y = .5
+for i in range(0, len(clt), 3):
     mean = (clt[i] + clt[i+1] + clt[i+2]) / 3
     temp = canvas.createtemplate()
     temp.scale(.5, "x")
@@ -19,11 +16,8 @@ for i in range(len(t)-1):
     else:
         move_x = .5
 
-    if i % 2 == 0:
-        if move_y == .5:
-            move_y = 0
-        elif move_y == 0:
-            move_y = .5
+    if i > 3:
+        move_y = 0
 
     temp.move(move_x, "x")
     temp.move(move_y, "y")
