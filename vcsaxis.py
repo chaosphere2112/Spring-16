8a12726b527bce303963c7a00185bd334a1886f1
Sam Fries <fries2@llnl.gov>
2016-03-02 11:22:20 -0800

    Added vcsaxis

---
+++ vcsaxis.py
@@ -0,0 +1,146 @@
+import vcs
+
+class VCSAxis(object):
+    def __init__(self, gm, tmpl, axis, left=0, right=0):
+        self.gm = gm
+        self.tmpl = tmpl
+        self._axis = axis
+        self.left = left
+        self.right = right
+
+    @property
+    def ticks(self):
+        """Use this attribute for the dict editor."""
+        if self._axis == "x1":
+            return self.gm.xticlabels1
+        elif self._axis == "x2":
+            return self.gm.xticlabels2
+        elif self._axis == "y1":
+            return self.gm.yticlabels1
+        elif self._axis == "y2":
+            return self.gm.yticlabels2
+
+    @ticks.setter
+    def ticks(self, val):
+        """Use this attribute for the dict editor."""
+        if self._axis == "x1":
+            self.gm.xticlabels1 = val
+        if self._axis == "x2":
+            self.gm.xticlabels2 = val
+        if self._axis == "y1":
+            self.gm.yticlabels1 = val
+        if self._axis == "y2":
+            self.gm.yticlabels2 = val
+
+    @property
+    def miniticks(self):
+        """Used internally."""
+        if self._axis == "x1":
+            return self.gm.xmtics1
+        elif self._axis == "x2":
+            return self.gm.xmtics2
+        elif self._axis == "y1":
+            return self.gm.ymtics1
+        elif self._axis == "y2":
+            return self.gm.ymtics2
+
+    @miniticks.setter
+    def miniticks(self, val):
+        """Used internally."""
+        if self._axis == "x1":
+            self.gm.xmtics1 = val
+        if self._axis == "x2":
+            self.gm.xmtics2 = val
+        if self._axis == "y1":
+            self.gm.ymtics1 = val
+        if self._axis == "y2":
+            self.gm.ymtics2 = val
+
+    @property
+    def mode(self):
+        if self.ticks == "*" or self.ticks in vcs.elements["list"]:
+            return "auto"
+        if self.step is not None:
+            return "even"
+        else:
+            return "manual"
+
+    @mode.setter
+    def mode(self, value):
+        if value == "auto" and self.ticks != "*" and self.ticks not in vcs.elements["list"]:
+            self.ticks = "*"
+        if value == "even":
+            self.ticks = {self.left: self.left, self.right: self.right}
+        if value == "manual":
+            self.ticks = {}
+
+    @property
+    def numticks(self):
+        if isinstance(self.ticks, dict):
+            return len(self.ticks)
+        return None
+
+    @numticks.setter
+    def numticks(self, num):
+        # Interpolate between left and right num times
+        step = (self.right - self.left) / float(num)
+        self.ticks = {self.left + n * step: self.left + n * step for n in range(num)}
+
+    @property
+    def show_miniticks(self):
+        """Use this attribute for the dict editor."""
+        if self._axis == "x1":
+            return self.tmpl.xmintic1.priority > 0
+        elif self._axis == "x2":
+            return self.tmpl.xmintic2.priority > 0
+        elif self._axis == "y1":
+            return self.tmpl.ymintic1.priority > 0
+        elif self._axis == "y2":
+            return self.tmpl.ymintic2.priority > 0
+
+    @show_miniticks.setter
+    def show_miniticks(self, val):
+        """Use this attribute for the dict editor."""
+        if self._axis == "x1":
+            self.tmpl.xmintic1.priority = 1 if val else 0
+        elif self._axis == "x2":
+            self.tmpl.xmintic2.priority = 1 if val else 0
+        elif self._axis == "y1":
+            self.tmpl.ymintic1.priority = 1 if val else 0
+        elif self._axis == "y2":
+            self.tmpl.ymintic2.priority = 1 if val else 0
+
+    @property
+    def minitick_count(self):
+        ticks = self.ticks
+        
+        axis_vals = sorted(ticks)
+        mini_ticks = sorted(self.miniticks)
+
+        last_val = axis_vals[0]
+        between_ticks = -1
+        for val in axis_vals[1:]:
+            this_between = 0
+            for i, tick in enumerate(mini_ticks):
+                if tick > val:
+                    mini_ticks = mini_ticks[i:]
+                    break
+                this_between += 1
+            if between_ticks == -1:
+                between_ticks = this_between
+            if this_between != between_ticks:
+                between_ticks = min(this_between, between_ticks)
+            last_val = val
+        return between_ticks
+
+    @minitick_count.setter
+    def minitick_count(self, count):
+        axis_vals = sorted(self.ticks)
+        prev_val = axis_vals[0]
+        miniticks = []
+        for val in axis_vals[1:]:
+            step = (val - prev_val) / float(count + 1)
+            miniticks.extend((prev_val + step * i for i in range(count + 1)))
+            prev_val = val
+        self.miniticks = {t: "" for t in miniticks}
+
